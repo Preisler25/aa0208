80e8e77b2da3d2de53f8c8d077fa6d35369e9695
Preisler25 <preisler.andras@gmail.com>
2024-02-08 15:44:35 +0100

    m

--- valaszok.sql
+++ valaszok.sql
@@ -1,2 +1,8 @@
 SELECT min(datum), max(datum) FROM `ertekesitesek`;
 
+SELECT eladok.nev, SUM(ertekesitesek.ertek) AS osszertek
+FROM eladok
+JOIN ertekesitesek ON eladok.eladoID = ertekesitesek.eladoID
+GROUP BY eladok.eladoID
+ORDER BY osszertek DESC;
+
